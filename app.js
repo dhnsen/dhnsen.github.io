8afaa34798d8d92fd8770d42e07d0c8ec2ff6b72
Dan H <spag1024@hotmail.com>
2019-07-13 18:06:51 -0500

    set up public directories, imported images and style.  removed EJS because i don't need it.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressLayouts = require('express-ejs-layouts');
 const nodemailer = require('nodemailer');
+const path = require('path');
 
 const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '/public/css')));
+app.use('/img', express.static(path.join(__dirname, '/public/img')));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
