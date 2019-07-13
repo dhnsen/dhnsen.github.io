fb8fad187aec5552afc57a44763e6f58a6809279
Dan H <spag1024@hotmail.com>
2019-07-13 18:20:47 -0500

    put ejs back in. decided it would be good for displaying confirmation or failure message

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
 const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/public/css')));
@@ -12,8 +15,6 @@ app.use('/img', express.static(path.join(__dirname, '/public/img')));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use('/', require(path.join(__dirname, '/routes/index')));
 
 app.listen(3000);
