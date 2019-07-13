e30a2240c3488742e255dac1a4d34a3f0189854a
Dan H <spag1024@hotmail.com>
2019-07-13 17:06:29 -0500

    installed dependencies

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const nodemailer = require('nodemailer');
+
+const app = express();
